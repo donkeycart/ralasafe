9b68ba63488e7c76675e044c8945dd96e3916b29
南风 <xueyj163@163.com>
2019-05-25 14:10:50 +0800

    初始化项目

---
+++ src/main/java/org/ralasafe/demo/Company.java
@@ -0,0 +1,33 @@
+package org.ralasafe.demo;
+
+public class Company {
+	private int id;
+	private String name;
+	private int parentId;
+	private int level;
+
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getParentId() {
+		return parentId;
+	}
+	public void setParentId( int parentId ) {
+		this.parentId=parentId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId( int id ) {
+		this.id=id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName( String name ) {
+		this.name=name;
+	}
+}
